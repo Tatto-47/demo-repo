3915769d12f100f12ec7b84e73a95e45231774b2
paul christian bakomen <paulchristianbakomen@pauls-MacBook-Air.local>
2023-05-15 17:04:31 -0400

    just to test making changes in commits

---
+++ ProgrammePrincipal.java
@@ -0,0 +1,13 @@
+/**
+ * Description : Cette classe est le point d'entrée du programme. 
+ *               Elle traite les arguments de la ligne de commande,
+ *               crée un objet FichierJSON pour lire le fichier d'entrée, 
+ *               crée des objets Employe, Intervention, Client, et Facture à partir des données lues, 
+ *               calcule l'état de compte pour chaque client et l'état de compte total, et utilise ensuite l'objet FichierJSON pour écrire ces informations dans le fichier de sortie.
+ */
+public class ProgrammePrincipal {
+    public static void main(String[] args) {
+
+       
+    }
+}
